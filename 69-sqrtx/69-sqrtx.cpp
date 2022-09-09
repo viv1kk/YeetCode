4040c10928ed057c6916b91de4c530f63b46cded
Vivek <vivekkohli935@gmail.com>
2022-09-09 15:29:04 +0530

    Time: 6 ms (40.19%), Space: 5.9 MB (73.79%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1 || x == 0)
+            return x;
+        long long int s = 0, e = x;
+        
+        while(s < e)
+        {
+            long long mid = s+((e-s)>>1);
+            long long int temp = mid*mid;
+            if(temp == x)
+                return mid;
+            else if(temp < x)
+                s = mid+1;
+            else
+                e = mid;
+        }
+        return s-1;
+    }
+};
