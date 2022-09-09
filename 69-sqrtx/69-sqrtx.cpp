6fc66e7b2725d81a026df04f2cd873aed5628054
Vivek <vivekkohli935@gmail.com>
2022-09-09 21:26:01 +0530

    Time: 7 ms (35.33%), Space: 5.9 MB (25.42%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -4,18 +4,19 @@ public:
         if(x == 1 || x == 0)
             return x;
         long int s = 0, e = x;
-        
-        while(s < e)
+        long int ans;
+        while(s <= e)
         {
             long mid = s+((e-s)>>1);
             long int temp = mid*mid;
-            if(temp == x)
-                return mid;
-            else if(temp < x)
+            if(temp <= x)
+            {
+                ans = mid;
                 s = mid+1;
+            }
             else
-                e = mid;
+                e = mid-1;
         }
-        return s-1;
+        return ans;
     }
 };
