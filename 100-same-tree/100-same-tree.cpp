26f88278bcd7cdd6739f9330eae8c47327d9846b
Vivek <vivekkohli935@gmail.com>
2022-09-13 14:13:30 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (6.71%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -17,10 +17,9 @@ public:
         if((p == NULL && q != NULL) || (p != NULL && q == NULL))
             return false;
         
-        if(p->val == q->val)
-        {
-            return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
-        }
-        return false;   
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
     }
 };
