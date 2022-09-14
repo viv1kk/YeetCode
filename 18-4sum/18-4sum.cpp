5b2bb8682454b67b826f0835f097029c43f81842
Vivek <vivekkohli935@gmail.com>
2022-09-15 01:10:01 +0530

    Time: 162 ms (33.18%), Space: 13 MB (93.51%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -9,44 +9,31 @@ public:
         sort(nums.begin(), nums.end());
         
         int s, e;
-        for(int i = 0; i < nums.size()-3; i++)
+        long x;
+        for(int i = 0; i < nums.size()-3; ++i)
         {
-            for(int j = i+1; j < nums.size()-2; j++)
+            for(int j = i+1; j < nums.size()-2; ++j)
             {
                 s = j+1, e = nums.size()-1;
              
                 while(s < e)
                 {
-                    long x = (long)target - ((long)nums[i]+(long)nums[j]+(long)nums[s]+(long)nums[e]);
+                    x = (long)target - ((long)nums[i]+(long)nums[j]+(long)nums[s]+(long)nums[e]);
                 
-                    if(x > 0)
-                    {
-                        ++s;
-                        
-                    }
-                    else if(x < 0)
-                    {
-                        e--;
-                    }
+                    if(x > 0)++s;
+                    else if(x < 0)e--;
                     else
                     {
-                        vector<int>temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[j]);
-                        temp.push_back(nums[s]);
-                        temp.push_back(nums[e]);
-                        ans.push_back(temp);
-
-
-                        while(s < e && nums[s] == nums[s+1])s++;
-                        while(e > s && nums[e] == nums[e-1])e--;
-                        s++;
-                        e--;
+                        ans.push_back({nums[i], nums[j], nums[s], nums[e]});
+                        while(s < e && nums[s] == nums[s+1])++s;
+                        while(e > s && nums[e] == nums[e-1])--e;
+                        ++s;
+                        --e;
                     }
                 }
-                while(j < nums.size()-2 && nums[j] == nums[j+1])j++;
+                while(j < nums.size()-2 && nums[j] == nums[j+1])++j;
             }
-            while(i < nums.size()-3 && nums[i] == nums[i+1])i++;
+            while(i < nums.size()-3 && nums[i] == nums[i+1])++i;
         }
         return ans;
     }
