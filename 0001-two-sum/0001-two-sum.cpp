10fee695d925c26632f0038111585409fede0407
Vivek <vivekkohli935@gmail.com>
2022-10-25 15:32:27 +0530

    Time: 28 ms (61.15%), Space: 10.9 MB (32.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -13,28 +13,28 @@ public:
         // return {};
         
         // using unordered_map
-        // unordered_map<int, int>mp;
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     int f = target-nums[i];
-        //     auto it = mp.find(f);
-        //     if(it == mp.end()) mp.insert({nums[i], i});
-        //     else return {i, it->second};
-        // }
-        // return {};
-        
-        //bruteforce
-        for(int i = 0; i < nums.size(); ++i)
+        unordered_map<int, int>mp;
+        for(int i = 0; i < nums.size(); i++)
         {
-            for(int j = i; j < nums.size(); ++j)
-            {
-                if(i == j) continue;
-                if(nums[i]+nums[j] == target)
-                {
-                    return {i, j};
-                }
-            }
+            int f = target-nums[i];
+            auto it = mp.find(f);
+            if(it == mp.end()) mp.insert({nums[i], i});
+            else return {i, it->second};
         }
         return {};
+        
+        //bruteforce
+        // for(int i = 0; i < nums.size(); ++i)
+        // {
+        //     for(int j = i; j < nums.size(); ++j)
+        //     {
+        //         if(i == j) continue;
+        //         if(nums[i]+nums[j] == target)
+        //         {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // return {};
     }
 };
