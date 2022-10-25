3003959f54dd28fd624ae14670e64a120edf7a34
Vivek <vivekkohli935@gmail.com>
2022-10-25 15:27:15 +0530

    Time: 879 ms (18.90%), Space: 10.2 MB (73.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,39 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // using map
+        // map<int, int>mp;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     int f = target-nums[i];
+        //     auto it = mp.find(f);
+        //     if(it == mp.end()) mp.insert({nums[i], i});
+        //     else return {i, it->second};
+        // }
+        // return {};
         
-        unordered_map<int, int>mp;
-        for(int i = 0; i < nums.size(); i++)
+        // using unordered_map
+        // unordered_map<int, int>mp;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     int f = target-nums[i];
+        //     auto it = mp.find(f);
+        //     if(it == mp.end()) mp.insert({nums[i], i});
+        //     else return {i, it->second};
+        // }
+        // return {};
+        
+        //bruteforce
+        for(int i = 0; i < nums.size(); ++i)
         {
-            int f = target-nums[i];
-            auto it = mp.find(f);
-            if(it == mp.end()) mp.insert({nums[i], i});
-            else return {i, it->second};
+            for(int j = i; j < nums.size(); ++j)
+            {
+                if(i == j) continue;
+                if(nums[i]+nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
         }
         return {};
-        
-        
     }
 };
