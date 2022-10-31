122348da6a37c22c385a446fdaa14df3add44d33
Vivek <vivekkohli935@gmail.com>
2022-10-31 09:43:27 +0530

    Time: 825 ms (73.96%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,3 +1,2 @@
 /* Write your PL/SQL query statement below */
-
-SELECT name AS Customers FROM Customers LEFT JOIN Orders ON Customers.id = Orders .customerId WHERE customerId IS NULL; 
+SELECT name AS Customers FROM Customers A LEFT JOIN Orders B ON A.id = B.customerId Where customerId IS NULL
