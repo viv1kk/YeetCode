a2bc2be4fd1d94cfb58515679dcc56c2294bd3bf
Vivek <vivekkohli935@gmail.com>
2022-10-31 08:47:36 +0530

    Time: 607 ms (14.31%), Space: 113.8 MB (45.29%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
-        mp.insert({0, -1}); // is the first element is a multiple of k then to compare put a initial value
-        
+        mp.insert({0,-1});
+        int rem;
         int sum = 0;
+        
         for(int i = 0; i < nums.size(); i++)
         {
             sum += nums[i];
-            int mod = sum%k;
+            rem = sum%k;
+            auto it = mp.find(rem);
             
-            auto it = mp.find(mod);
-            if(it == mp.end())
-                mp.insert({mod, i});
-            else
-            {
-                if(i - it->second >= 2)
-                    return true;
-                else
-                    mp.insert({mod, i});
-            }
+            if(it != mp.end() && i-it->second >= 2)
+                return true;
+            mp.insert({rem, i});
         }
         return false;
     }
