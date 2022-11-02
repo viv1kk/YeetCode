a1821673e95ce842274081a19b5bd5b33c6dba31
Vivek <vivekkohli935@gmail.com>
2022-11-02 11:33:56 +0530

    Time: 552 ms (32.05%), Space: 103.6 MB (67.85%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -6,27 +6,17 @@ public:
         {
             auto it = mp.find(t[i]);
             if(it == mp.end())
-            {
                 mp.insert({t[i], 1});
-            }
             else
-            {
                 it->second += 1;
-            }
         }
                           
         int r = 0;
         for(auto it=mp.begin(); it!=mp.end(); it++)
         {
             if(it->second == 1) return -1;
-            else if((it->second)%3 == 0)
-            {
-                r+=(it->second)/3;
-            }
             else
-            {
-                r+=(it->second)/3+1;
-            }
+                r+=((it->second)+2)/3;
         }
         return r;
     }
