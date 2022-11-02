7b5b07a69ea75478825fccd8c921bd364e81730f
Vivek <vivekkohli935@gmail.com>
2022-11-02 15:39:26 +0530

    Time: 246 ms (90.69%), Space: 103.6 MB (67.85%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int minimumRounds(vector<int>& t) {
-        unordered_map<int, int>mp;
-        for(int i = 0; i < t.size(); i++)
-        {
-            auto it = mp.find(t[i]);
-            if(it == mp.end())
-            {
-                mp.insert({t[i], 1});
-            }
-            else
-            {
-                it->second += 1;
-            }
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < tasks.size(); ++i){
+            mp[tasks[i]]++;
         }
-                          
         int r = 0;
-        for(auto it=mp.begin(); it!=mp.end(); it++)
-        {
-            if(it->second == 1) return -1;
-            else if((it->second)%3 == 0)
-            {
-                r+=(it->second)/3;
+        for(auto it = mp.begin(); it != mp.end(); ++it){
+            int f = it->second;
+            if(f == 1) return -1;
+            // else if(f % 3 == 0){
+            //     r += f/3;
+            // }
+            // else{
+            //     r += (f/3)+1;
+            // }
+            else{
+                r+=(f+2)/3;
             }
-            else
-            {
-                r+=((it->second)/3)+1;
-            }
+            
         }
         return r;
     }
