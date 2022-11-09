410f23e76fa94ab9ed831bed29a5af22d83f7e04
Vivek <vivekkohli935@gmail.com>
2022-11-09 09:29:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (65.54%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int i = 0;
+        while(i+1 < s.length())
+        {
+            if(abs(s[i]-s[i+1]) == 32)
+            {
+                s.erase(s.begin()+i);
+                s.erase(s.begin()+i);
+                i = 0;
+                continue;
+            }
+            i++;
+        }
+        return s;
+    }
+};
