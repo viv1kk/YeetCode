18ca6e0cdd76f5da6bd6eb8a2c90181e31a73f2d
Vivek <vivekkohli935@gmail.com>
2022-11-11 02:06:36 +0530

    Time: 19 ms (85.06%), Space: 10.7 MB (79.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,8 +4,8 @@ public:
         int s1 = text1.length();
         int s2 = text2.length();
         
-        vector<vector<int> >dp(s1+1, vector<int>(s2+1, 0));
-        
+        // vector<vector<int> >dp(s1+1, vector<int>(s2+1, 0));
+        int dp[1001][1001];
         for(int i = 1; i <= s1; i++)
         {
             for(int j = 1; j <= s2; j++)
