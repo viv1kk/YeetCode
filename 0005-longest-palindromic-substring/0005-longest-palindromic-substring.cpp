c78a9be6637901ca1df4ccc2c1708d766e2bf7c6
Vivek <vivekkohli935@gmail.com>
2022-11-11 00:19:00 +0530

    Time: 20 ms (84.26%), Space: 6.7 MB (96.50%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,58 +1,34 @@
 class Solution {
 public:
-    bool isPalindrome(string &s)
-    {
-        for(int i = 0, j = s.length()-1; i < j; i++, j--)
-        {
-            if(s[i] != s[j]) return false;
-        }
-        return true;
-    }
-    int findPalindromeOflen(int x, string &s)
-    {
-        for(int i = 0; i+x <= s.length(); i++)
-        {
-            string t = s.substr(i, x);
-            if(isPalindrome(t)) return i;
-        }
-        return -1;
-    }
-    
     string longestPalindrome(string s) {
         int n = s.length();
         int best_len = 0;
         int best_ind = 0;
         
-        for(int i : {0, 1})
+        for(int mid = 0; mid < n; mid++)
         {
-            int l = 1, r = n;
-            
-            if(l%2 != i) l++;
-            if(r%2 != i) r--;
-            
-            while(l <= r)
+            for(int j = 0; mid-j+1 >= 0 && mid+j < n; j++)
             {
-                int mid = l+(r-l)/2;
-                if(mid%2 != i) mid++;
-                
-                if(mid > r)break;
-                
-                int ind = findPalindromeOflen(mid, s);
-                
-                if(ind != -1)
+                if(s[mid-j+1] != s[mid+j]) break;
+                int len = 2*j;
+                if(len > best_len)
                 {
-                    if(best_len < mid)
-                    {
-                        best_len = mid;
-                        best_ind = ind;
-                    }
-                    l = mid+2;
+                    best_len = len;
+                    best_ind = mid-j+1;
                 }
-                else
-                    r = mid-2;
-                
+            }
+            for(int j = 0; mid-j >= 0 && mid+j < n; j++)
+            {
+                if(s[mid-j] != s[mid+j]) break;
+                int len = 2*j+1;
+                if(len > best_len)
+                {
+                    best_len = len;
+                    best_ind = mid-j;
+                }
             }
         }
+        
         string ans = s.substr(best_ind, best_len);
         return ans;
     }    
