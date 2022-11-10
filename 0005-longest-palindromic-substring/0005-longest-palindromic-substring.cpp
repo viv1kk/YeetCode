2c97eb3ff11460897506e9d077f42302aeb4562e
Vivek <vivekkohli935@gmail.com>
2022-11-10 17:40:30 +0530

    Time: 185 ms (45.92%), Space: 96.1 MB (27.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,49 +1,66 @@
-bool is_palindrome(string s) {
-    string rev = s;
-    reverse(rev.begin(), rev.end());
-    return s == rev;
+bool isPalindrome(string str)
+{
+    int s = str.length();
+    for(int i = 0, j = s-1; i < j; i++, j--)
+    {
+        if(str[i] != str[j])
+            return false;
+    }
+    return true;
 }
-// returns true if there is a palindrome of length x
-int good(int x, string s) {
-    int n = s.length();
-    for(int L = 0; L + x <= n; L++) {
-        if(is_palindrome(s.substr(L, x))) {
-            return L;
-        }
+
+int checkPalindromeOfSize(int s, string str)
+{
+    for(int i = 0; i+s <= str.length(); i++){
+        if(isPalindrome(str.substr(i, s)))
+            return i;
     }
     return -1;
 }
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int best_len = 0;
-        string best_s = "";
-        int n = s.length();
-        for(int parity : {0, 1}) {
-            int low = 1, high = n;
-            if(low % 2 != parity) low++;
-            if(high % 2 != parity) high--;
-            while(low <= high) {
-                int mid = (low + high) / 2;
-                if(mid % 2 != parity) {
-                    mid++;
+
+
+string longestPalindrome(string s) {
+    int n = s.length();
+
+    int best_len = 0;
+    string best_str = "";
+
+
+    //if i == 0 find even length palindromic substring 
+    //if i == 1 find odd length plalindromic substring
+    for(int i = 0; i <= 1; i++)
+    {
+        int S = 1, E = n;
+
+        if((S&1) != i)S++;
+        if((E&1) != i)E--;
+
+        while(S <= E)
+        {
+            int mid = S+(E-S)/2;
+
+            if((mid&1) != i)mid++;
+            if(mid > E) break;
+
+            // check wheather there is a palindromic substring of length m 
+            int ind = checkPalindromeOfSize(mid, s);
+            
+            if(ind != -1)
+            {
+                if(mid > best_len)
+                {
+                    best_len = mid;
+                    best_str = s.substr(ind, mid);
                 }
-                if(mid > high) {
-                    break;
-                }
-                int tmp = good(mid, s);
-                if(tmp != -1) {
-                    if(mid > best_len) {
-                        best_len = mid;
-                        best_s = s.substr(tmp, mid);
-                    }
-                    low = mid + 2;
-                }
-                else {
-                    high = mid - 2;
-                }
+                S = mid + 2;
             }
-        }
-        return best_s;
+            else
+            E = mid - 2;
+        }   
     }
+    return best_str;
+}
+
 };
