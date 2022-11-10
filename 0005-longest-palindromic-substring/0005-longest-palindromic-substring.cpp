0733b0d5cc6f68b0f574ab8e7528af4aeeab42e7
Vivek <vivekkohli935@gmail.com>
2022-11-11 00:01:40 +0530

    Time: 307 ms (32.19%), Space: 94.2 MB (28.14%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,59 @@
 class Solution {
 public:
+    bool isPalindrome(string &s)
+    {
+        for(int i = 0, j = s.length()-1; i < j; i++, j--)
+        {
+            if(s[i] != s[j]) return false;
+        }
+        return true;
+    }
+    int findPalindromeOflen(int x, string &s)
+    {
+        for(int i = 0; i+x <= s.length(); i++)
+        {
+            string t = s.substr(i, x);
+            if(isPalindrome(t)) return i;
+        }
+        return -1;
+    }
+    
     string longestPalindrome(string s) {
         int n = s.length();
-        vector<vector<bool>>dp(n, vector<bool>(n, false));
+        int best_len = 0;
+        int best_ind = 0;
         
-        int ind = 0, max = 0;
-        for(int dist = 0; dist < n; dist++)
+        for(int i : {0, 1})
         {
-            for(int i = 0, j = i+dist; i+dist < n; i++, j++)
+            int l = 1, r = n;
+            
+            if(l%2 != i) l++;
+            if(r%2 != i) r--;
+            
+            while(l <= r)
             {
-                if(i==j) dp[i][j] = true;
-                if(dist == 1 && s[i] == s[j]) dp[i][j] = true;
-                if(dist > 1 && s[i] == s[j] && dp[i+1][j-1] == true) dp[i][j] = true;
+                int mid = l+(r-l)/2;
+                if(mid%2 != i) mid++;
                 
-                if(dp[i][j] && max < dist)
+                if(mid > r)break;
+                
+                int ind = findPalindromeOflen(mid, s);
+                
+                if(ind != -1)
                 {
-                    max = dist;
-                    ind = i;
+                    if(best_len < mid)
+                    {
+                        best_len = mid;
+                        best_ind = ind;
+                    }
+                    l = mid+2;
                 }
+                else
+                    r = mid-2;
+                
             }
         }
-        string ans = s.substr(ind, max+1);
+        string ans = s.substr(best_ind, best_len);
         return ans;
-    }
+    }    
 };
