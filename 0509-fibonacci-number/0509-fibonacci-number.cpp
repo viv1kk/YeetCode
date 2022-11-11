ede9e9a1c54130c53a9769ead046e229bfe67447
Vivek <vivekkohli935@gmail.com>
2022-11-11 11:58:44 +0530

    Time: 50 ms (6.22%), Space: 6 MB (37.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,9 @@
-#include <math.h>
 class Solution {
 public:
-    int dp[31];
+    int dp[31] = {0};
     int fib(int n) {
         if(n < 2)
-            return n;
-        dp[0] = 0;
-        dp[1] = 1;
-        if(dp[n] != 0)
-            return dp[n];
+            return dp[n] = n;
         return dp[n] = fib(n-1)+fib(n-2);
     }
 };
