47457f051c8620a10dc12833c23156b11218dd88
Vivek <vivekkohli935@gmail.com>
2022-11-13 01:31:41 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (62.33%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-        bool dp[1001][1001] = {false};
-        int d = s.length();
-        
-        int count = 0;
-        for(int dist = 0; dist < d; dist++)
+    int countSubstrings(string str) {
+        string s = "$#";
+        for(int i = 0; i < str.length(); i++)
         {
-            for(int i = 0, j = i+dist; j < d; i++, j++)
-            {
-                if(i == j)dp[i][j] = true;
-                if(dist == 1 && s[i]==s[j]) dp[i][j] = true;
-                if(dist > 1 && s[i]==s[j] && dp[i+1][j-1]) dp[i][j] = true;
-                if(dp[i][j])count++;
-            }
+            s.push_back(str[i]);
+            s.push_back('#');
+        }
+        s.push_back('@');
+
+        int d = s.length();
+        vector<int>p(d,0);
+
+        int l = 0, c = 0, r = 0, mx = 0;
+        for(int i = 1; i < d-2; i++)
+        {
+            if(i<r) p[i] = min(r-i, p[(2*c-i)]);
+            while(s[(i-p[i]-1)]==s[(i+p[i]+1)])p[i]++;
+            if(i+p[i]>r)c=i,r=i+p[i];
+        }
+        int count = 0;
+        for(int i = 0; i < d; i++)
+        {
+            if(i%2) count=count+(p[i]/2);
+            else count=count+(p[i]+1)/2;;
         }
         return count;
     }
