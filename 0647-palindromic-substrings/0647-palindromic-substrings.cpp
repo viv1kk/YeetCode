7460384570c0b52f42e7dafc9c0e701836f6144c
Vivek <vivekkohli935@gmail.com>
2022-11-13 00:05:38 +0530

    Time: 23 ms (47.14%), Space: 7.4 MB (58.05%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        bool dp[1001][1001] = {false};
+        
+        int d = s.length();
+        int count = 0;
+        for(int dist = 0; dist < d; ++dist)
+        {
+            for(int i = 0, j = i+dist; j < d; ++j, ++i)
+            {
+                if(i == j) dp[i][j] = true;
+                if(dist == 1 && s[i] == s[j]) dp[i][j] = true;
+                if(dist > 1 && s[i] == s[j] && dp[i+1][j-1]) dp[i][j] = true;
+                if(dp[i][j])count++;
+            }
+        }
+        return count;
+    }
+};
