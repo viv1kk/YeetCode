da60c8a069062a4b49778d5c8b6a6e9b7329cb31
Vivek <vivekkohli935@gmail.com>
2022-11-12 19:56:01 +0530

    Time: 138 ms (27.88%), Space: 33.1 MB (39.70%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -4,11 +4,11 @@ public:
         set<int> s1(nums1.begin(), nums1.end()), s2(nums2.begin(), nums2.end());
         vector<vector<int>>ans(2);
         
-        for(auto i : s1)
+        for(int i : s1)
         {
             if(s2.find(i) == s2.end()) ans[0].push_back(i);
         }
-        for(auto i : s2)
+        for(int i : s2)
         {
             if(s1.find(i) == s1.end()) ans[1].push_back(i);
         }        
