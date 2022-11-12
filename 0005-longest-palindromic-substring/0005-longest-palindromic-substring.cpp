682fffa7f029a800394b55a68b199933ce84fae6
Vivek <vivekkohli935@gmail.com>
2022-11-12 11:40:11 +0530

    Time: 22 ms (81.10%), Space: 8.2 MB (61.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-                int n = s.length();
 
-        int dp[1001][1001]={0};
-        int mx = 0;
-        int id = 0;
-                // vector<vector<bool>>dp(n, vector<bool>(n, false));
+string longestPalindrome(string str) {
+    string s= "";
+    s.push_back('@');
+    for(int i = 1, j = 0; i < (2*str.length())+2; i++)
+    {
+        if((i&1)== 1)
+            s.push_back('#');
+        else
+            s.push_back(str[j++]);
+    }
 
-        
-        for(int dist = 0; dist < s.length(); dist++)
-        {
-            for(int i = 0, j = i+dist; i+dist < s.length(); i++, j++)
-            {
-                if(i==j) dp[i][j] = 1;
-                if(dist == 1 && s[i]==s[j]) dp[i][j] = true;
-                if(dist > 1 && s[i] == s[j] && dp[i+1][j-1] == true) dp[i][j] = true;
-                
-                if(dp[i][j] && mx< dist)
-                {
-                    mx = dist;
-                    id = i;
-                }
-            }
+    s.push_back('$');
+    // cout<<s;
+    // cout<<s.length()<<endl;
+    // cout<<str.length()<<endl;
+
+    int strLen = s.length();
+    int maxLen = 0;
+    int start = 0;
+    int maxRight = 0;
+    int center = 0;
+
+    vector<int>p(strLen, 0);
+    for(int i = 1;i < strLen-1; i++)
+    {
+        if(i < maxRight)
+            p[i] = min(maxRight-i, p[((2*center) -i)]);
+        while(s[(i+p[i]+1)] == s[(i-p[i]-1)])
+            p[i]++;
+        if(i+p[i] > maxRight){
+            center = i; maxRight = i+p[i];
         }
-        string ans = s.substr(id, mx+1);
-        return ans;
+        if(p[i] > maxLen){
+            start = (i-p[i]-1)/2;
+            maxLen = p[i];
+        }
+    }
+    // cout<<endl<<maxLen;
+    string ans = str.substr(start, maxLen);
+    // cout<<endl;
+    return ans;
     }
 };
