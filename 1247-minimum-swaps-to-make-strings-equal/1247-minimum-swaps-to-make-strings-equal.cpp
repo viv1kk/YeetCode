b6c4e0087ea6d4cbfff59bd207188fdf3e44d7cb
Vivek <vivekkohli935@gmail.com>
2022-11-20 07:14:49 +0530

    Time: 3 ms (52.26%), Space: 6.2 MB (72.03%) - LeetHub

--- 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
@@ -17,34 +17,7 @@ public:
         
         if((x+y)%2 == 1) return -1;
         int ans = (x+y)/2;
-        if (x%2 == 1)
-        {
-            ans+=1;
-        }
+        if(x%2 == 1) ans+=1;
         return ans;
-
-//      // int n=s1.length();
-//      //    if(n!=s2.length())return -1;
-//      //    int hash[26]={0};
-//      //    for(auto i:s1){
-//      //        hash[i-'0']++;
-//      //    }
-//      //    for(auto i:s2){
-//      //        hash[i-'0']++;
-//      //    }
-//      //    for(auto i:hash){
-//      //        if(i&1)return -1;
-//      //    }
-//      //    int cnt=0;
-//      //    for(int i=0;i<n;i++){
-//      //        if(s1[i]!=s2[i]){
-//      //            int j=i+1;
-//      //            while(j<n && s1[i]!=s2[j])j++;
-//      //            if(j==n)return -1;
-//      //            swap(s[i],s[j]);
-//      //            cnt++;
-//      //        }
-//      //    }
-//      //    return cnt;
     }
 };
