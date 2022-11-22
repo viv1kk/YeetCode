b3402618f0b34ffa7f20df7aa74f87912f495458
Vivek <vivekkohli935@gmail.com>
2022-11-22 11:02:24 +0530

    Time: 190 ms (42.59%), Space: 8 MB (63.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string r = s;
-        r = "$#";
-        for(auto i : s)
-        {
-            r += i;
-            r += '#';
-        }
-        r +='@';
-        int n = r.length();
-        vector<int>p(n, 0);
+        bool dp[1001][1001] = {false};
         
-        int l = 0, e = 0, c = 0, mx = 0;
-        for(int i = 1; i < n-2; i++)
+        int n = s.length();
+        int mx = 0;
+        int ind = 0;
+        for(int d = 0 ; d < n; d++)
         {
-            if(i < e)
+            for(int i = 0, j = i+d; j < n; i++, j++)
             {
-                p[i] = min(e-i, p[2*c-i]);
-            }
-            while(r[i+p[i]+1] == r[i-p[i]-1])p[i]++;
-            
-            if(p[i]+i > e)
-            {
-                c = i;
-                e = i+p[i];
-            }
-            if(mx < p[i])
-            {
-                l = (i-p[i]-1)/2;
-                mx = p[i];
+                if(i == j)dp[i][j] = true;
+                if(d == 1 && s[i] == s[j]) dp[i][j] = true;
+                if(d > 1 && s[i] == s[j] && dp[i+1][j-1]) dp[i][j] = true;
+                
+                if(d > mx && dp[i][j]){
+                    ind = i;
+                    mx = d;
+                }
             }
         }
-        string st = s.substr(l, mx);
+        string st = s.substr(ind, mx+1);
         return st;
     }
 };
