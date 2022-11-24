96707ba69d46e734703fa3961f72c83171dfa965
Vivek <vivekkohli935@gmail.com>
2022-11-24 18:22:19 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (50.22%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -4,8 +4,7 @@ public:
         int n = s.length();
         for(int i = 1; i < n; i+=2)
         {
-            int temp = s[i]-'0';
-            s[i] = s[i-1]+temp;
+            s[i] = s[i-1]+(s[i]-'0');
         }
         return s;
     }
