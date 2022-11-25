3a91c9c3e390b1397e6faa16b53c9c402e69718d
Vivek <vivekkohli935@gmail.com>
2022-11-25 15:48:04 +0530

    Time: 268 ms (33.10%), Space: 11.4 MB (50.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        bool dp[1001][1001] = {false};
+        int dp[1001][1001] = {0};
+        string r = s;
+        reverse(r.begin(), r.end());
         
         int n = s.length();
         int mx = 0;
         int ind = 0;
-        for(int d = 0 ; d < n; d++)
+        for(int d = 0; d < n; d++)
         {
             for(int i = 0, j = i+d; j < n; i++, j++)
             {
-                if(i == j)dp[i][j] = true;
-                if(d == 1 && s[i] == s[j]) dp[i][j] = true;
-                if(d > 1 && s[i] == s[j] && dp[i+1][j-1]) dp[i][j] = true;
+                if(i == j) dp[i][j] = 1;
+                if(d == 1 && s[i] == s[j]) dp[i][j] = 1;
+                if(d > 1 && s[i] == s[j] && dp[i+1][j-1] == 1) dp[i][j] = 1;
                 
-                if(d > mx && dp[i][j]){
-                    ind = i;
+                if(dp[i][j] == 1 && mx < d)
+                {
                     mx = d;
+                    ind = i;
                 }
             }
         }
-        string st = s.substr(ind, mx+1);
-        return st;
+        string ans = s.substr(ind, mx+1);
+        return ans;
     }
 };
