903fb3d2439c655d8a6e3d5b4a23c2ca474858a5
Vivek <vivekkohli935@gmail.com>
2023-02-10 14:39:45 +0530

    Time: 1150 ms (5.03%), Space: 18.4 MB (88.63%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -42,11 +42,11 @@ public:
     void f(vector<vector<int>>& grid, vector<vector<int>>&dp, int i, int j, int x, int n)
     {
         if(x >= dp[i][j]) return;
-        dp[i][j] = x;
-        if(i+1 < n && grid[i+1][j] == 0) f(grid, dp, i+1, j, x+1,n);
-        if(i > 0 && grid[i-1][j] == 0) f(grid, dp, i-1, j, x+1,n);
-        if(j+1 < n && grid[i][j+1] == 0) f(grid, dp, i, j+1, x+1,n);
-        if(j > 0 && grid[i][j-1] == 0) f(grid, dp, i, j-1, x+1,n);
+        dp[i][j] = x++;
+        if(i+1 < n && grid[i+1][j] == 0 && dp[i+1][j] > x) f(grid, dp, i+1, j, x,n);
+        if(i > 0 && grid[i-1][j] == 0 && dp[i-1][j] > x) f(grid, dp, i-1, j, x,n);
+        if(j+1 < n && grid[i][j+1] == 0 && dp[i][j+1] > x) f(grid, dp, i, j+1, x,n);
+        if(j > 0 && grid[i][j-1] == 0 && dp[i][j-1] > x) f(grid, dp, i, j-1, x,n);
     }
     int maxDistance(vector<vector<int>>& grid)
     {
