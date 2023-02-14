12c9ff408ac12ee4e03cfc0ed82dac9550959f8a
Vivek <vivekkohli935@gmail.com>
2023-02-14 19:53:43 +0530

    Time: 3 ms (66.26%), Space: 6 MB (7.77%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int a = low%2;
-        int b = high%2;
-        
         int ans = 0;
-        if(a == 1)
+        if(low&1)
         {
             ans++;
             low++;
         }
-        if(b == 1)
+        if(high&1)
         {
             ans++;
             high--;
         }
-        ans += (high-low)/2;
+        ans += (high-low)>>1;
         return ans;
     }
 };
