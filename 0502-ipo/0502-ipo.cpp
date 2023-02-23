5d82be806fc08642f5caa985cc34fe006571d7fb
Vivek <vivekkohli935@gmail.com>
2023-02-24 00:51:53 +0530

    Time: 345 ms (19.12%), Space: 84.3 MB (19.89%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -12,30 +12,26 @@ public:
     
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> cpq;
-        priority_queue<pair<int, int>,vector<pair<int,int>>, comp>ppq;
+        // priority_queue<pair<int, int>,vector<pair<int,int>>, comp>ppq;
+        priority_queue<pair<int, int>>ppq;
         
         for(int i = 0; i < profits.size(); i++)
         {
             cpq.push({capital[i], profits[i]});
         }
         
-        // for(int  i = 0; i < profits.size(); i++)
-        // {
-        //     cout<<cpq.top().first<<" "<<cpq.top().second<<endl;
-        //     cpq.pop();
-        // }
-        
         int x = w;
         while(k--)
         {
             while(!cpq.empty() && cpq.top().first <= x)
             {
-                ppq.push(cpq.top());
+                auto[x, y] = cpq.top();
+                ppq.push({y, x});
                 cpq.pop();
             }
             if(!ppq.empty())
             {
-                x += ppq.top().second;
+                x += ppq.top().first;
                 ppq.pop();
             }
         }
