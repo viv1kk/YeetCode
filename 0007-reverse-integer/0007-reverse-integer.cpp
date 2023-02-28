f02b336a0cfe8a12590a788e27d6a39e372f6bff
Vivek <vivekkohli935@gmail.com>
2023-02-28 18:04:13 +0530

    Time: 3 ms (57.09%), Space: 5.9 MB (92.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    void f(string &s)
-    {
-        std::reverse(s.begin(), s.end());
-    }
     int reverse(int x) {
-        string s = to_string(x);
         bool sign = true;
-        
-        if(s[0] == '-')
+        if(x < 0)
             sign = false;
-        
-        // if(sign == false)
-        // {
-        //     // s.push_back('-');
-        // }
-        f(s);
-        if(sign == false)
-        {
-            s.pop_back();
-        }
-        f(s);
-        cout<<s<<endl;
         long ans = 0;
-        int i = s.length();
-        while(i)
+        
+        while(x)
         {
-
+            int rem = abs(x)%10;
             ans *= 10;
-            ans = (abs(ans)+s[--i]-'0');
+            ans = (abs(ans)+rem);
             if(ans >= 0  && sign == false)
             {
                 ans *= -1;
             }
+            x /= 10;
             if(ans > INT_MAX || ans < INT_MIN)
                 return 0;
         }
