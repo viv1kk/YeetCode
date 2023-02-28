53ac6d11e29d78ae908c591cb515f16f200b43a9
Vivek <vivekkohli935@gmail.com>
2023-02-28 21:57:56 +0530

    Time: 14 ms (63.15%), Space: 12.9 MB (20.37%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,35 +1,31 @@
+class comp
+{
+public:
+     bool operator()(pair<int,string> a, pair<int,string> b) const{
+        if(a.first < b.first) return true;
+        else if(a.first == b.first && a.second>b.second) return true;
+        return false;
+    }
+};
+
+
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int>mp;
         for(auto i : words)
             mp[i]++;
-        priority_queue<pair<int, string>>pq;
+        
+        priority_queue<pair<int, string>, vector<pair<int,string>>, comp >pq;
         for(auto[a, b] : mp)
             pq.push({b, a});
 
-        priority_queue<pair<string, int>, vector<pair<string, int>>, greater<pair<string, int>> >pq2;
-        
         vector<string>ans;
-        int count = k;
-        while(!pq.empty())
+        
+        while(k--)
         {
-            auto[a, b] = pq.top();
-            pq2.push({b, a});
+            ans.push_back(pq.top().second);
             pq.pop();
-            while(!pq.empty() && pq.top().first == a)
-            {
-                auto[c, d] = pq.top();
-                pq2.push({d, c});
-                pq.pop();
-            }
-            while(!pq2.empty() && count)
-            {
-                ans.push_back(pq2.top().first);
-                pq2.pop();
-                count--;
-            }
-            if(count == 0) break;
         }
         return ans;   
     }
