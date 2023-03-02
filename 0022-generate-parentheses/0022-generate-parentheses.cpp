7bc3d5e8147be37ef1411896d6a45df10c4bc3df
Vivek <vivekkohli935@gmail.com>
2023-03-03 00:19:15 +0530

    Time: 77 ms (5.06%), Space: 74.1 MB (5.02%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -17,10 +17,15 @@ public:
             if(stk.empty()) ans.push_back(a);
             return;
         }
-        
+        stack<char>stk;
+        for(int i = 0; i < a.length(); i++)
+        {
+            if(stk.empty() && a[i] == ')') return;
+            if(a[i] == '(') stk.push('(');
+            else if(!stk.empty()&& stk.top() == '(' && a[i] == ')')stk.pop(); 
+        }
         f(n-1,a+'(');        
         f(n-1, a+')');
-        
     }
     
     vector<string> generateParenthesis(int n) {
