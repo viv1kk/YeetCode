92caeabf46cfd79844797cd70fb882cc0e3e0938
Vivek <vivekkohli935@gmail.com>
2023-03-03 00:12:32 +0530

    Time: 3 ms (92.73%), Space: 9 MB (42.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
     int compress(vector<char>& c) {
-        int n = c.size();
-        
-        for(int i = 0; i < n; )
+        int n = c.size(), i = 0;
+        while(i < n)
         {
             int j = i;
             int count = 1;
             while(j+1 < n && c[j+1] == c[i]){ j++; count++; }
             if(count == 1) { i++; continue;}
-            
             string s = to_string(count);
-            
-            for(auto k : s)
-            {
-                c[i+1] = k;
-                i++;
-            }
-            while(i+1 <= j)
-            {
-                c[i+1] = '\0';
-                i++;
-            }
+            for(auto k : s){ c[i+1] = k; i++; }
+            while(i+1 <= j){ c[i+1] = '\0'; i++; }
             i++;
         }
         int p=0, q= 0;
-        
         while(p < n && q < n)
         {
-            if(c[p] !='\0' && c[q] != '\0')
-            {
-                p++;
-                q++;
-            }
-            else if(c[q] != '\0')
-            {
-                swap(c[p++], c[q++]);
-            }
-            else if(c[p] == '\0')
-            {
-                q++;
-            }
+            if(c[p] !='\0' && c[q] != '\0'){ p++; q++; }
+            else if(c[q] != '\0') swap(c[p++], c[q++]);
+            else if(c[p] == '\0') q++;
         }
         while(c.back()== '\0'){ c.pop_back(); }
         return (int)c.size();
