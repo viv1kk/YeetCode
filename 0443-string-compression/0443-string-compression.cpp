be471139f19f8ab0c2ea9c88ea23f1597cad6b46
Vivek <vivekkohli935@gmail.com>
2023-03-03 00:03:06 +0530

    Time: 10 ms (26.84%), Space: 8.9 MB (98.13%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int compress(vector<char>& c) {
+        int n = c.size();
+        
+        for(int i = 0; i < n; )
+        {
+            int j = i;
+            int count = 1;
+            while(j+1 < n && c[j+1] == c[i]){ j++; count++; }
+            if(count == 1) { i++; continue;}
+            
+            string s = to_string(count);
+            
+            for(auto k : s)
+            {
+                c[i+1] = k;
+                i++;
+            }
+            while(i+1 <= j)
+            {
+                c[i+1] = '\0';
+                i++;
+            }
+            i++;
+        }
+        int p=0, q= 0;
+        
+        while(p < n && q < n)
+        {
+            if(c[p] !='\0' && c[q] != '\0')
+            {
+                p++;
+                q++;
+            }
+            else if(c[q] != '\0')
+            {
+                swap(c[p++], c[q++]);
+            }
+            else if(c[p] == '\0')
+            {
+                q++;
+            }
+        }
+        while(c.back()== '\0'){ c.pop_back(); }
+        return (int)c.size();
+    }
+};
