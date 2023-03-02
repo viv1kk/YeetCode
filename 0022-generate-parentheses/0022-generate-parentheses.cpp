08c320dc7b5af91a5f86f8d39391bd6d7a6faba4
Vivek <vivekkohli935@gmail.com>
2023-03-02 11:03:55 +0530

    Time: 182 ms (5.06%), Space: 173.5 MB (5.02%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    vector<string>ans;
+    
+    void f(int n,  string a)
+    {
+        if(n == 0)
+        {
+            stack<char>stk;
+            for(int i = 0; i < a.length(); i++)
+            {
+                if(stk.empty() && a[i] == ')') return;
+                if(a[i] == '(') stk.push('(');
+                else if(!stk.empty()&& stk.top() == '(' && a[i] == ')')stk.pop(); 
+            }
+            if(stk.empty()) ans.push_back(a);
+            return;
+        }
+        
+        f(n-1,a+'(');        
+        f(n-1, a+')');
+        
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        f((2*n), "");
+        return ans;
+    }
+};
