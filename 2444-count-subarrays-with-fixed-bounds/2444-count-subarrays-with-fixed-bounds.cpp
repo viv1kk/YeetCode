3cbbd63201255e8c8af3cd1b6b99542ac0c82baf
Vivek <vivekkohli935@gmail.com>
2023-03-04 22:42:48 +0530

    Time: 120 ms (71.84%), Space: 70.5 MB (57.55%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-       long res = 0;
-        bool minFound = false, maxFound = false;
-        int start = 0, minStart = 0, maxStart = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            if (num < minK || num > maxK) {
-                minFound = false;
-                maxFound = false;
-                start = i+1;
-            }
-            if (num == minK) {
-                minFound = true;
-                minStart = i;
-            }
-            if (num == maxK) {
-                maxFound = true;
-                maxStart = i;
-            }
-            if (minFound && maxFound) {
-                res += (min(minStart, maxStart) - start + 1);
-            }
+        // minPosition, maxPosition: the MOST RECENT positions of minK and maxK.
+        // leftBound: the MOST RECENT value outside the range [minK, maxK].
+        long long answer = 0;
+        int minPosition = -1, maxPosition = -1, leftBound = -1;
+        
+        // Iterate over nums, for each number at index i:
+        for (int i = 0; i < nums.size(); ++i) {
+            // If the number is outside the range [minK, maxK], update the most recent leftBound.
+            if (nums[i] < minK || nums[i] > maxK)
+                leftBound = i;
+            
+            // If the number is minK or maxK, update the most recent position.
+            if (nums[i] == minK) 
+                minPosition = i;
+            if (nums[i] == maxK)
+                maxPosition = i;
+
+            // The number of valid subarrays equals the number of elements between leftBound and 
+            // the smaller of the two most recent positions (minPosition and maxPosition).
+            answer += max(0, min(maxPosition, minPosition) - leftBound);
         }
-        return res;
+        return answer;
     }
 };
