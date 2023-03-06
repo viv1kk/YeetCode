f8ba03978da8b7ca055a7c8c260d3df5baf9e30a
Vivek <vivekkohli935@gmail.com>
2023-03-06 20:37:18 +0530

    Time: 3 ms (41.34%), Space: 6 MB (10.06%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.cpp
+++ 1201-ugly-number-iii/1201-ugly-number-iii.cpp
@@ -4,15 +4,19 @@ public:
     ll lcm(ll a, ll b){
         return (a*b)/gcd(a, b);
     }
+    
+    ll f(int x, ll a, ll b, ll c){
+        return x/a + x/b + x/c - x/lcm(a,b) - x/lcm(b,c) - x/lcm(a,c) + x/lcm(a, lcm(b,c));  
+    }
     int nthUglyNumber(int n, int a, int b, int c) {
         ll l = 0;
         ll h = INT_MAX;
         
         while(l <= h)
         {
-            ll x = l+(h-l)/2;
-            if((x/a + x/b + x/c - x/lcm(a,b) - x/lcm(b,c) - x/lcm(a,c) + x/lcm(a, lcm(b,c))) >= n) h = x-1;
-            else l = x+1;
+            ll mid = l+(h-l)/2;
+            if(f(mid, a, b, c) >= n) h = mid-1;
+            else l = mid+1;
         }
         return l;
     }
