d6e64266716828253c84a1ae498dfb7c7a3be405
Vivek <vivekkohli935@gmail.com>
2023-03-06 11:20:21 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (12.85%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<int>>pq(nums.begin(), nums.end());
-        
-        int mn = 0, count = 0;
-        while(!pq.empty())
-        {
-            if(pq.top() == 0) pq.pop();
-            else{
-                int x = pq.top()-mn;
-                if(x != 0)
-                {
-                    mn += x;
-                    count++;
-                }
-                pq.pop();
-            }
-        }
+        set<int>st(nums.begin(), nums.end());
+        int count = st.size();
+        if(st.find(0) != st.end())
+            count--;
         return count;
     }
 };
