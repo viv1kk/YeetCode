5400b3a70e2793c41fe7b202458e36b65f06a47e
Vivek <vivekkohli935@gmail.com>
2023-03-08 14:11:48 +0530

    Time: 10 ms (86.19%), Space: 11.5 MB (27.71%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -4,17 +4,8 @@ public:
         unordered_map<string, string>mp;
         for(auto i : paths)
              mp[i[0]] = i[1];
-        
         string t = paths[0][1];
-        while(true)
-        {
-            if(mp.find(t) == mp.end())
-            {
-                return t;
-            }
-            else
-                t = mp[t];
-        }
-        return "";
+        while(mp.find(t) != mp.end()) t = mp[t];
+        return t;
     }
 };
