c2f2a13222936680828de0004ad58befd30db303
Vivek <vivekkohli935@gmail.com>
2023-03-10 21:59:33 +0530

    Time: 1756 ms (6.20%), Space: 286.9 MB (7.18%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -5,10 +5,10 @@ public:
         
         for(int i = 0; i < words.size(); i++)
         {
-            unordered_set<char>a(words[i].begin(), words[i].end());
+            set<char>a(words[i].begin(), words[i].end());
             for(int j = i+1; j < words.size(); j++)
             {
-                unordered_set<char>b(words[j].begin(), words[j].end());
+                set<char>b(words[j].begin(), words[j].end());
                 if(a == b)
                     count++;
             }
