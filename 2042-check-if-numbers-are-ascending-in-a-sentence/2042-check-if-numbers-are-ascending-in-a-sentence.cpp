cf68e9d82ddd0242c0331ee439affd44ac4b1194
Vivek <vivekkohli935@gmail.com>
2023-03-12 23:05:47 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.61%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static bool comp(int a, int b)
+    {
+        return a <= b;
+    }
+    bool areNumbersAscending(string s) {
+        vector<int>a;
+        
+        s += ' ';
+        string t = "";
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == ' ' && t != "")
+            {
+                a.push_back(stoi(t));
+                t = "";
+            }
+            if(s[i] >= '0' && s[i] <= '9')
+                t+=s[i];   
+        }
+        return is_sorted(a.begin(), a.end(), comp);
+    }
+};
