0cafedab261c8ad2bc917ff9b1e10c1905c06513
Vivek <vivekkohli935@gmail.com>
2023-03-16 21:50:25 +0530

    Time: 115 ms (16.71%), Space: 24.9 MB (90.58%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -8,7 +8,7 @@ public:
         for(int i = 0; i < n; i++){
             if(ans == ""){ ans += s[i]; continue; }
             char x = ans.back();
-            if((x != '*' && s[i] == '*')||(x == '*' && s[i] != '*')) ans.pop_back();
+            if(!(x != '*' ^ s[i] == '*')) ans.pop_back();
             else ans += s[i];
         }
         return ans;
