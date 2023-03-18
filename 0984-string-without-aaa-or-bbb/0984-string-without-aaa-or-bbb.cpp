dce8f0a17e58d60efc4f2607118b2c56b1acd5dd
Vivek <vivekkohli935@gmail.com>
2023-03-19 00:53:38 +0530

    Time: 4 ms (36.63%), Space: 6.1 MB (51.07%) - LeetHub

---
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    string strWithout3a3b(int a, int b) {
+        priority_queue<pair<int, char>>pq;
+        if(a) pq.push({a, 'a'});
+        if(b) pq.push({b, 'b'});
+        
+        pair<int, char>prev, curr;
+        string s = "";
+        
+        while(!pq.empty())
+        {
+            pair<int, char>temp;
+            temp = {0, 'd'};
+            
+            curr = pq.top();
+            if(curr == prev)
+            {
+                temp = curr;
+                pq.pop();
+                if(!pq.empty()){
+                    curr = pq.top();
+                }
+            }
+            pq.pop();
+            
+            int r = 0;
+            if(temp.second == 'd') r = min(2, curr.first);
+            else
+            {
+                int x = curr.first;
+                int y = (int)ceil((double)temp.first+1/(double)x);
+                if(y >= 3) r = 1;
+                else r = min(2, min(y, curr.first));
+                pq.push(temp);
+            }
+            s += curr.second;
+            if(r > 1) s += curr.second;
+            
+            curr.first = curr.first- r;
+            
+            if(curr.first > 0)
+                pq.push(curr);
+            prev = curr;
+        }
+        
+        if(s == ""){
+            if(a) s.insert(0, min(a, 2), 'a');
+            if(b) s.insert(0, min(b, 2), 'b');
+            // if(c) s.insert(0, min(c, 2), 'c');
+        }
+        else if(!pq.empty() && s.back() != pq.top().second)
+        {
+            int x = min(2, pq.top().first);
+            s += pq.top().second;
+            if(x > 1) s+=pq.top().second;
+        }
+        return s;
+    }
+};
