33c959db53230043ae28f782a63a972ffde9c575
Vivek <vivekkohli935@gmail.com>
2023-03-20 11:37:27 +0530

    Time: 87 ms (92.26%), Space: 58.9 MB (92.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        vector<int>left(n);
-        vector<int>right(n);
+        int l = 0, r = height.size()-1;
+        int ans = 0;
         
-        int mx = 0;
-        
-        for(int i = 0 ; i < n; i++)
+        while(l < r)
         {
-            mx = max(mx, height[i]);
-            left[i] = mx;
-        }
-        
-        mx = 0;
-        for(int i = n-1; i >= 0; i--)
-        {
-            mx = max(mx, height[i]);
-            right[i] = mx;
-        }
-        
-//         for(int i : left)
-//             cout<<i<<", ";
-//         cout<<endl;
-        
-//         for(int i : right)
-//             cout<<i<<", ";
-//         cout<<endl;
-        
-        int i =0 , j = n-1;
-        mx = 0;
-        while(i < j)
-        {
-            // cout<<mx<<" ";
-            if(left[i] < right[j])
+            ans = max(ans, (r-l)*min(height[l], height[r]));
+            if(height[l] > height[r])
             {
-                mx = max(mx, left[i]*(j-i));
-                i++;
+                r--;
             }
             else
             {
-                mx = max(mx, right[j]*(j-i));
-                j--;
+                l++;
             }
+                
         }
-        
-        return mx;
+        return ans;
     }
 };
