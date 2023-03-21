1be3596852f626ed9cf995b655187508ee92f548
Vivek <vivekkohli935@gmail.com>
2023-03-22 00:51:21 +0530

    Time: 3 ms (93.95%), Space: 6.2 MB (37.81%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,76 +1,58 @@
 class Solution {
 public:
-    unordered_map<int, char>mp;
 
-        
     string convert(int x)
     {
         string t = "";
-        
-    cout<<x<<endl;
-            if(x%5 == 4)
-            {
-                t += 'I';
-                x++;
-            }
-            if(x%50 == 40)
-            {
-                t += 'X';
-                x+=10;
-            }
-            if(x%500 == 400)
-            {
-                t += 'C';
-                x+=100;
-            }
+        if(x%5 == 4)
+        {
+            t += 'I';
+            x++;
+        }
+        if(x%50 == 40)
+        {
+            t += 'X';
+            x+=10;
+        }
+        if(x%500 == 400)
+        {
+            t += 'C';
+            x+=100;
+        }
 
-            while(x >= 1000){
-                t+='M';
-                x -= 1000;
-            }
-            while(x >= 500 && x < 1000){
-                t +='D';
-                x-=500;
-            }
-            while(x >= 100 && x < 500){
-                t +='C';
-                x-=100;
-            }
-            while(x >= 50 && x < 100){
-                t +='L';
-                x-=50;
-            }
-            while(x >= 10 && x < 50){
-                t +='X';
-                x-=10;
-            }
-            while(x >= 5 && x < 10){
-                t += 'V';
-                x -= 5;
-            }
-            while(x > 0 && x < 5){
-                t.insert(t.size(), x, 'I');
-                x = 0;
-            }
-        // if(mp.find(x) != mp.end())
-        //     {
-        //         t += mp[x];
-        //         x -= mp[x];
-        //     }
-        
+        while(x >= 1000){
+            t+='M';
+            x -= 1000;
+        }
+        while(x >= 500 && x < 1000){
+            t +='D';
+            x-=500;
+        }
+        while(x >= 100 && x < 500){
+            t +='C';
+            x-=100;
+        }
+        while(x >= 50 && x < 100){
+            t +='L';
+            x-=50;
+        }
+        while(x >= 10 && x < 50){
+            t +='X';
+            x-=10;
+        }
+        while(x >= 5 && x < 10){
+            t += 'V';
+            x -= 5;
+        }
+        while(x > 0 && x < 5){
+            t.insert(t.size(), x, 'I');
+            x = 0;
+        }
         return t;
     }
     
     string intToRoman(int num) {
-        string ans = "";
-        mp[1] = 'I';
-        mp[5] = 'V';
-        mp[10] = 'X';
-        mp[50] = 'L';
-        mp[100] = 'C';
-        mp[500] = 'D';
-        mp[1000] = 'M';
-        
+        string ans = "";        
         string n = to_string(num);
         int i = 0;
         
@@ -82,7 +64,6 @@ public:
             x /= 10;
             i++;
         }
-        
         return ans;
     }
 };
