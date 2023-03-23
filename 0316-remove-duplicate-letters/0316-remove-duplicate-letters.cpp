1073901284088d1becee6e534824ef31161dd195
Vivek <vivekkohli935@gmail.com>
2023-03-23 21:50:01 +0530

    Time: 3 ms (72.63%), Space: 6.4 MB (95.06%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         string ans = "";
-        bool vis[26] = {false};
-        int freq[26] = {0};
+        bool vis[255] = {false};
+        int freq[255] = {0};
         int n = s.length();
         
         for(char&i : s){
-            freq[i-'a']++;
+            freq[i]++;
         }
         
         for(int i = 0; i < n; i++)
         {
-            freq[s[i]-'a']--;
-            if(vis[s[i]-'a']) continue;
-            while(!ans.empty() && ans.back()>s[i] && freq[ans.back()-'a'] > 0){
-                vis[ans.back()-'a'] = false;
+            freq[s[i]]--;
+            if(vis[s[i]]) continue;
+            while(!ans.empty() && ans.back()>s[i] && freq[ans.back()] > 0){
+                vis[ans.back()] = false;
                 ans.pop_back();
             }
-            vis[s[i]-'a'] = true;
+            vis[s[i]] = true;
             ans += s[i];
         }
         return ans;
