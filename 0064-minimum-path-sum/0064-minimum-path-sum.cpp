95fd7132d8f0d4a402ea346d96cc46d46d2064e3
Vivek <vivekkohli935@gmail.com>
2023-03-27 23:09:40 +0530

    Time: 8 ms (79.94%), Space: 10.1 MB (61.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     
-    void f(vector<vector<int>>&grid, vector<vector<int>>&dp, int i, int j, int m, int n, int sum)
-    {
-        if(i >= m || j >= n) return;
-        if(i == m-1 && j == n-1){
-            dp[i][j] = min(dp[i][j], grid[i][j]+sum);
-        }
-        sum += grid[i][j];
-        if(dp[i][j] <= sum)
-            return;
-        dp[i][j] = min(dp[i][j], sum);
-        f(grid, dp, i+1, j, m, n, sum);
-        f(grid, dp, i, j+1, m, n, sum);
-    }
+    // void f(vector<vector<int>>&grid, vector<vector<int>>&dp, int i, int j, int m, int n, int sum)
+    // {
+    //     if(i >= m || j >= n) return;
+    //     if(i == m-1 && j == n-1){
+    //         dp[i][j] = min(dp[i][j], grid[i][j]+sum);
+    //     }
+    //     sum += grid[i][j];
+    //     if(dp[i][j] <= sum)
+    //         return;
+    //     dp[i][j] = min(dp[i][j])
+    //     f(grid, dp, i+1, j, m, n, sum);
+    //     f(grid, dp, i, j+1, m, n, sum);
+    // }
     
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(); 
-        int n = grid[0].size();
-        vector<vector<int>>dp(m, vector<int>(n, INT_MAX));
-        
-        f(grid, dp, 0, 0,m, n, 0);
-        
-        return dp[m-1][n-1];    
+    int minPathSum(vector<vector<int>>& gr) {
+        vector<vector<int>> dp(gr.size(),vector<int>(gr[0].size(),-1));
+        for(int i=0;i<gr.size();i++){
+            for(int j=0; j<gr[0].size();j++){
+                if(i==0 && j==0) dp[i][j] = gr[i][j];
+                else{
+                    int a=1e7,b=1e7;
+                    if(i>0)
+                        a= dp[i-1][j];
+                    if(j>0)
+                        b = dp[i][j-1];
+                    dp[i][j] = gr[i][j] + min(a,b);
+                }
+            }
+        }
+        return dp[gr.size()-1][gr[0].size()-1];
     }
 };
