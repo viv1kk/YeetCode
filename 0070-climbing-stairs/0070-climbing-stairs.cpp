9a2350eddf4609aba5bb27169a2f121fe1a1281e
Vivek <vivekkohli935@gmail.com>
2023-03-29 00:00:31 +0530

    Time: 4 ms (9.88%), Space: 6.3 MB (21.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int f(int n, int c, vector<int>&dp)
+    {
+        if(n < c) return 0;
+        if(n == c) return 1;
+        if(dp[c] != 0) return dp[c];
+        int x = 0;
+        return dp[c]=(f(n, c+2, dp)+f(n, c+1,dp));
+    }
+    
+    int climbStairs(int n) {
+        vector<int>dp(n, 0);
+        return f(n, 0, dp);
+    }
+};
