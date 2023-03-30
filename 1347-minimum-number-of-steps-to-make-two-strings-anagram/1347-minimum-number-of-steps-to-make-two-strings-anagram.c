609dbfb3cca869baec9f6d5f28dcb7417def8526
Vivek <vivekkohli935@gmail.com>
2023-03-30 19:39:32 +0530

    Time: 22 ms (83.33%), Space: 8.3 MB (100.00%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.c
@@ -0,0 +1,12 @@
+int minSteps(char * s, char * t){
+    int f[127] = {0};
+    int n = 0;
+    while(s[n] !='\0') f[s[n++]]++;
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(f[t[i]] == 0) count++;
+        else f[t[i]]--;
+    }
+    return count;
+}
