488c83bbd2bf73973cb1d0119d0d6e1a0b2d61b3
Vivek <vivekkohli935@gmail.com>
2023-04-01 09:47:17 +0530

    Time: 35 ms (90.99%), Space: 7.1 MB (47.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.c
+++ 0704-binary-search/0704-binary-search.c
@@ -1,12 +1,22 @@
 int search(int* nums, int numsSize, int target){
     int s = 0, e = numsSize-1, mid = 0;
-    
+    int ans = -1;
     while(s <= e)
     {
         mid = s+((e-s)>>1);
-        if(*(nums+mid) > target) e = mid-1;
-        else if(*(nums+mid) < target) s = mid+1;
-        else return mid;
+        if (target==*(nums+mid)){
+            ans=mid;
+            break;
+        }
+        else if (target<*(nums+mid)){
+            e=mid-1;
+        }
+        else if (target>*(nums+mid)){
+            s=mid+1;
+        }
+        else{
+            break;
+        }
     }
-    return -1;
+    return ans;
 }
