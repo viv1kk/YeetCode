654956bce0973300d56d7594bc6f30471e35ff05
Vivek <vivekkohli935@gmail.com>
2023-04-06 14:22:35 +0530

    Time: 7 ms (98.51%), Space: 9.5 MB (77.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    
-    bool flag = true;
-    void f(vector<vector<int>>&grid, int i, int j, int &m, int &n)
+    bool f(vector<vector<int>>&grid, int i, int j, int &m, int &n)
     {
-        if(i >= m || j >= n || i < 0 || j < 0)
-        {
-            flag = false;
-            return;
-        }
-        if(grid[i][j] == 1)return;        
+        if(i >= m || j >= n || i < 0 || j < 0) return false;
+        if(grid[i][j] == 1)return true;
+        
         int temp = grid[i][j];
         grid[i][j] = 1;
-        
-        if(flag)f(grid, i+1, j, m, n);
-        if(flag)f(grid, i-1, j, m, n);
-        if(flag)f(grid, i, j+1, m, n);
-        if(flag)f(grid, i, j-1, m, n);
-        if(!flag)grid[i][j] = temp;
+        bool x =  f(grid, i+1, j, m, n) & f(grid, i-1, j, m, n) & f(grid, i, j+1, m, n) & f(grid, i, j-1, m, n);
+        if(!x)grid[i][j] = temp;
+        return x;
     }
     
     int closedIsland(vector<vector<int>>& grid) {
@@ -27,22 +19,11 @@ public:
         
         for(int i = 1; i < m-1; i++){
             for(int j = 1; j < n-1; j++){
-                if(grid[i][j] == 0){
-                    f(grid, i, j, m, n);
-                    if(flag ==  true)
+                if(grid[i][j] == 0)
+                    if(f(grid, i, j, m, n))
                         ans++;
-                }
-                flag = true;
             }
         }
-        // for(int i = 1; i < grid.size()-1; i++)
-        // {
-        //     for(int j = 1; j < grid[0].size()-1; j++)
-        //     {
-        //         cout<<grid[i][j]<<"\t";
-        //     }
-        //     cout<<endl;
-        // }
         return ans;
     }
 };
