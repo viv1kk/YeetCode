0e4eab5add720bd88a9ca8248deaf267bc2e8092
Vivek <vivekkohli935@gmail.com>
2023-04-06 23:13:50 +0530

    Time: 47 ms (25.34%), Space: 14.4 MB (46.26%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int m = matrix.size(), n = matrix[0].size();
-        priority_queue<int, vector<int>, greater<int>>pq;
+        priority_queue<int>pq;
         
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
                 pq.push(matrix[i][j]);
+                if(pq.size() == k+1){
+                    pq.pop();
+                }
             }
         }
-        int ans = 0;
-        while(k--)
-        {
-            ans = pq.top();
-            pq.pop();
-        }
-        return ans;
+        return pq.top();
     }
 };
