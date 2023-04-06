f0e43d96a3782e3e0f906ea9f0f9e50f59498278
Vivek <vivekkohli935@gmail.com>
2023-04-06 13:48:37 +0530

    Time: 9 ms (92.15%), Space: 9.4 MB (77.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     
-    int ans = 0;
     bool flag = true;
-    
-    void f(vector<vector<int>>&grid, int i, int j)
+    void f(vector<vector<int>>&grid, int i, int j, int m, int n)
     {
-        if(i >= grid.size() || j >= grid[0].size() || i < 0 || j < 0)
+        if(i >= m || j >= n || i < 0 || j < 0)
         {
             flag = false;
             return;
         }
-        // if(grid[i][j] == -1) return;
-        if(grid[i][j] == 1)
-            return;
-
-        
+        if(grid[i][j] == 1)return;        
         int temp = grid[i][j];
-        
         grid[i][j] = 1;
-        if(flag)f(grid, i+1, j);
-        if(flag)f(grid, i-1, j);
-        if(flag)f(grid, i, j+1);
-        if(flag)f(grid, i, j-1);
+        
+        if(flag)f(grid, i+1, j, m, n);
+        if(flag)f(grid, i-1, j, m, n);
+        if(flag)f(grid, i, j+1, m, n);
+        if(flag)f(grid, i, j-1, m, n);
         if(!flag)grid[i][j] = temp;
     }
     
     int closedIsland(vector<vector<int>>& grid) {
-        for(int i = 1; i < grid.size()-1; i++)
-        {
-            for(int j = 1; j < grid[0].size()-1; j++)
-            {
-                if(grid[i][j] == 0)
-                {
-                    f(grid, i, j);
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for(int i = 1; i < m-1; i++){
+            for(int j = 1; j < n-1; j++){
+                if(grid[i][j] == 0){
+                    f(grid, i, j, m, n);
                     if(flag ==  true)
                         ans++;
                 }
