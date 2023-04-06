db12c2f4f39837707814ff6e93a3f69f5d47ee2b
Vivek <vivekkohli935@gmail.com>
2023-04-07 00:15:18 +0530

    Time: 27 ms (90.55%), Space: 13.1 MB (95.57%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n = matrix.size();
+    int kthSmallest(vector<vector<int>>& m, int k) {
+        int n = m.size();
+        int l = m[0][0], r = m[n-1][n-1];
         
-        int l = matrix[0][0], r = matrix[n-1][n-1];
-        int ele = INT_MIN;
-        int ans = -1;
-        
+        int ans = 0;
         while(l <= r)
         {
             int mid = l+(r-l)/2;
             int count = 0;
             for(int i = 0; i < n; i++)
             {
-                count += upper_bound(matrix[i].begin(), matrix[i].end(), mid)-matrix[i].begin();
+                int x = 0, y = n-1;
+                while(x <= y)
+                {
+                    int d = x+(y-x)/2;
+                    if(m[i][d] > mid)
+                        y = d-1;
+                    else
+                        x = d+1;
+                }
+                count += x;
             }
-            
-            if (count >= k) {
+            if(count >= k)
+            {
                 ans = mid;
-                r = mid - 1; // try to looking for a smaller value in the left side
-            } else l = mid + 1;
+                r = mid-1;
+            }
+            else 
+                l = mid+1;
         }
         return ans;
     }
