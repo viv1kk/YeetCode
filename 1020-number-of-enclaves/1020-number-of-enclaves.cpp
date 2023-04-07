58307fd5c856c074e7507a9ceea7a87ac6e0518d
Vivek <vivekkohli935@gmail.com>
2023-04-07 15:35:31 +0530

    Time: 63 ms (97.72%), Space: 31.1 MB (42.61%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    void f(vector<vector<int>>&grid, int i, int j)
+    {
+        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] == 0) return;
+        grid[i][j] = 0;
+        
+        f(grid, i+1, j);
+        f(grid, i-1, j);
+        f(grid, i, j+1);
+        f(grid, i, j-1);
+    }
+    
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+        int m = grid.size(), n = grid[0].size();
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if((i == 0 || j == 0 || i == m-1 || j == n-1)&& grid[i][j] == 1)
+                    f(grid, i, j);
+            }
+        }
+        int ans = 0;   
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                ans += grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
