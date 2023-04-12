10e6e94520fe260ffa50dc661894ae0910e472df
Vivek <vivekkohli935@gmail.com>
2023-04-13 03:34:10 +0530

    Time: 11 ms (18.53%), Space: 7.7 MB (88.22%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>st;
+        
+        string s = "";
+        path += '/';
+        for(int i = 0; i < path.size(); i++)
+        {
+            if(s != "" && path[i] == '/')
+            {
+                int x = count(s.begin(), s.end(), '.');
+                if(x == s.length())
+                {
+                    if(x < 3)
+                    {
+                        if(!st.empty() && x == 2) st.pop();
+                        s = "";
+                    }
+                    else
+                    {
+                        st.push(s);
+                    }
+                }
+                else
+                {
+                    st.push(s);
+                }
+                s = "";
+            }
+            else
+            {
+                if(path[i] == '/')continue;
+                else
+                {
+                    s += path[i];
+                }
+            }
+        }
+        string ans = "";
+        while(!st.empty())
+        {
+            ans.insert(0, st.top());
+            ans.insert(0, "/");
+            st.top();
+            st.pop();
+        }
+        if(ans == "")
+            ans = "/";
+        return ans;
+    }
+};
