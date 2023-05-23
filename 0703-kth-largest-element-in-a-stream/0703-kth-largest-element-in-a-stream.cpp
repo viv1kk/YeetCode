f49804a24eeda351bda6f507e9ac45ad43df62cd
Vivek <vivekkohli935@gmail.com>
2023-05-23 20:31:17 +0530

    Time: 42 ms (26.25%), Space: 19.8 MB (60.44%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,20 +1,23 @@
 class KthLargest {
 public:
-    priority_queue<int, vector<int>, greater<int>>pq;
-    int x;
+    priority_queue<int, vector<int>, greater<int> >pq;
+    int k;
     KthLargest(int k, vector<int>& nums) {
-        for(auto i : nums)
+        for(int i : nums)
         {
             pq.push(i);
             if(pq.size() > k)
                 pq.pop();
         }
-        x = k;
+        this->k = k;
     }
     
     int add(int val) {
         pq.push(val);
-        if(pq.size() > x) pq.pop();
+       if(pq.size() > k)
+       {
+           pq.pop();
+       }
         return pq.top();
     }
 };
