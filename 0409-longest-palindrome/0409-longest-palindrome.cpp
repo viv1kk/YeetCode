f7cb4d938779993dcb8bcc6d29709e7791d45a6d
Vivek <vivekkohli935@gmail.com>
2024-01-27 01:59:00 +0530

    Time: 3 ms (55.60%), Space: 9 MB (6.31%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<char, int>mp;
+        for(char i : s) mp[i]++;
+        
+        int odd = 0;
+        int tot = 0;
+        for(auto&[i, j] : mp){
+            if(j%2) odd++;
+            tot += j;
+        }
+        
+        int ans = 0;
+        if(odd > 1) ans = tot - odd + 1;
+        else ans = tot;
+        return ans;
+    }
+};
