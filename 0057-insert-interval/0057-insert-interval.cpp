e4c1565938b2ff60dcbc73615a7bdb2c56080696
Vivek <vivekkohli935@gmail.com>
2024-01-31 01:42:30 +0530

    Time: 4 ms (97.59%), Space: 20.5 MB (8.45%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -6,13 +6,13 @@ public:
         int n = a.size();
         
         while(idx < n && a[idx][1] < ni[0]) ans.push_back(a[idx++]);
-        int l = 0, r = 0;
+        int l = ni[0], r = ni[1];
         while(idx < n && a[idx][0] <= ni[1]){
-            ni[0] = min(a[idx][0], ni[0]);
-            ni[1] = max(a[idx][1], ni[1]);
+            l = min(a[idx][0], l);
+            r = max(a[idx][1], r);
             idx++;
         }
-        ans.push_back(ni);
+        ans.push_back({l, r});
         
         while(idx < n) ans.push_back(a[idx++]);
         return ans;
