6bdcfedecfce72d7816a614d64e54a0a848acc4b
Vivek <vivekkohli935@gmail.com>
2024-02-08 11:19:24 +0530

    Time: 530 ms (7.01%), Space: 156.4 MB (5.32%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int f(vector<int>&v, int ind, int n, int s, vector<vector<int>>&dp){
+        if(n == 0) return 0;
+        if(n < 0) return 1000;
+        if(ind == s) return 1000;
+        if(dp[ind][n] != -1)return dp[ind][n];
+        return dp[ind][n] = min(1+f(v, ind, n-v[ind], s, dp), f(v, ind+1, n, s, dp));
+    }
+    
+    int numSquares(int n) {
+        vector<int>v(n+1, 0);
+        
+        vector<int>pf;
+        for(int i = 1; i <= n; i++)
+        {
+            if(i*i <= n){
+                v[i*i] = 1;
+            }
+            if(v[i]){
+                pf.push_back(i);
+            }
+        }
+        
+        vector<vector<int>>dp(pf.size(),vector<int>(n+1, -1));
+        for(int i : pf)cout<<i<<", ";
+        return f(pf, 0, n, pf.size(), dp);
+    }
+};
