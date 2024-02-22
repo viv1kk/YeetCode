4f06c5fc4e9507cd63d2d08412469d8dbbb17051
Vivek <vivekkohli935@gmail.com>
2024-02-22 22:29:20 +0530

    Time: 201 ms (11.94%), Space: 82.6 MB (5.08%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n == 1 && trust.size() == 0) return 1;
+        map<int, set<int>>mp;
+        map<int, int>mp2;
+        
+        for(auto& i : trust){
+            mp[i[1]].insert(i[0]);
+            mp2[i[0]] = i[1];
+        }
+        for(auto&[i, j] : mp){
+            if(j.size() == n-1 && j.find(i) == j.end() && mp2.find(i) == mp2.end())
+                return i;
+        }
+        return -1;
+    }
+};
