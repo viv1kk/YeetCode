14de8e4d1c698d574545ac4d26d11384e66aef01
Vivek <vivekkohli935@gmail.com>
2024-02-24 23:00:18 +0530

    Time: 126 ms (7.14%), Space: 15.8 MB (67.98%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<pair<int, int>>> adj;
-        for (auto& flight : flights) {
-            adj[flight[0]].push_back({flight[1], flight[2]});
-        }
-
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        int stops = 0;
-
-        while (!q.empty() && stops <= k) {
-            int sz = q.size();
-            while (sz-- > 0) {
-                auto [node, distance] = q.front();
-                q.pop();
-
-                if (!adj.count(node)) continue;
-
-                for (auto& [neighbour, price] : adj[node]) {
-                    if (price + distance >= dist[neighbour]) continue;
-                    dist[neighbour] = price + distance;
-                    q.push({neighbour, dist[neighbour]});
-                }
+    int dp[101][101];
+    
+    int f(vector<vector<int>>&flights, int n, int src, int dst, int k){
+        if(src == dst) return 0;
+        if(k <= 0) return 1e9;
+        if(dp[src][k] != -1) return dp[src][k];
+        
+        int ans = 1e9;
+        for(int i = 0; i < n; i++){
+            if(flights[i][0] == src){
+                ans = min(ans, flights[i][2]+f(flights, n, flights[i][1], dst, k-1));
             }
-            stops++;
         }
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        return dp[src][k] = ans;
+    }
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        memset(dp, -1, sizeof dp);
+        int ans = f(flights, flights.size(), src, dst, k+1);
+        if (ans >= 1e9) return -1;
+        return ans;
     }
 };
